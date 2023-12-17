cfdacc425536b74c439fc58b135e9a38f99ad05e
wolfspecter <57796399+wolfspecter@users.noreply.github.com>
2023-12-17 18:23:35 +0700

    Add files via upload

---
+++ systemc/supply.cpp
@@ -0,0 +1,20 @@
+﻿#include "supply.h"
+
+void Supply::supplyProcess() {
+    while (true) {
+            // Simulate water supply (this could be modeled based on real-time)
+            waterAvailable.write(true);
+            SC_REPORT_INFO("Supply", "Water is available");
+            water = true;
+            wait(15, SC_NS);  // Assume water is available for 10 seconds
+            waterAvailable.write(false);
+            SC_REPORT_INFO("Supply", "Out of water");
+            water = false;
+            wait(15, SC_NS);  // Assume out of water for the next 20 seconds
+            SC_REPORT_INFO("Supply", "Water is available");
+            water = true;
+            wait(15, SC_NS);  // Assume water is available for 10 seconds
+            waterAvailable.write(true);
+            wait();
+    }
+}
---
+++ systemc/testbench.cpp
@@ -0,0 +1,48 @@
+#include "testbench.h"
+
+void Testbench::testProcess() {
+    reset.write(true);
+    reset.write(false);
+
+    // Test 1: Normal operation
+    SC_REPORT_INFO("Testbench", "Test 1: Normal operation");
+    coinInserted.write(30000);
+    sensor.write(true);
+    wait(1, SC_NS);
+    sensor.write(false);
+    selectedDrink.write(WATER);
+    return_money.write(false);
+    wait(14, SC_NS);
+
+    //Test 2: Out of water
+    SC_REPORT_INFO("Testbench", "Test 2: Out of water");
+    coinInserted.write(10000);
+    sensor.write(true);
+    wait(1, SC_NS);
+    sensor.write(false);
+    selectedDrink.write(WATER);
+    return_money.write(false);
+    wait(14, SC_NS);
+
+    // Test 3: Reset and coin inserted
+    SC_REPORT_INFO("Testbench", "Test 3: Reset and coin inserted");
+    reset.write(true);
+    reset.write(false);
+    coinInserted.write(20000);
+    sensor.write(true);
+    wait(1, SC_NS);
+    sensor.write(false);
+    selectedDrink.write(COFFEE);
+    wait(2, SC_NS);
+    coinInserted.write(30000);
+    sensor.write(true);
+    wait(1, SC_NS);
+    sensor.write(false);
+    return_money.write(true);
+    selectedDrink.write(COFFEE);
+    wait(10, SC_NS);
+    
+
+    // Add more test scenarios as needed
+    sc_stop(); // Stop simulation after the tests
+}
